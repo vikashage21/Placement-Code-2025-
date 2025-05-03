653c5a45a39f4479a566765ec46a4dfdf5f38e5b
vikash sharma <vk220783@gmail.com>
2025-05-03 17:29:55 +0530

    Array-done

---
+++ pw_course/javascript/03.js
@@ -0,0 +1,19 @@
+// learning array
+
+let arr = [1, 2, 3];
+
+console.log(arr)
+// you can also create array by new Array();
+
+let newArr = new Array(10, 20.5, "hello world")
+console.log(newArr)
+
+// given array -> [1, 2, 3]
+// indexes / pos => [0, 1, 2] last element element index = arr.length -1;
+
+// updating array values by index . if given index is not valid then its return undefined
+
+// array data is mutable - it can be change . 
+arr[2] = 10;
+console.log(arr)
+
