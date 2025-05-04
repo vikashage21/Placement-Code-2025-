544eb7ff1d3605731c4c93b5c92770bd8a911651
vikash sharma <vk220783@gmail.com>
2025-05-04 14:34:07 +0530

    function in js - parameter and argument

---
+++ pw_course/javascript/05_.js
@@ -0,0 +1,24 @@
+// functions in js
+
+function greet(){
+    console.log("Hello World");
+}
+
+// creating a square root of function
+
+// parameter
+
+function sqr (x){ 
+    return Math.sqrt(x);
+}
+
+
+greet()
+
+
+let a = sqr(8) 
+
+// argument is 8
+
+
+console.log(a)
