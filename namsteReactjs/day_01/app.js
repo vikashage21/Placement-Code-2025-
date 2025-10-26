0249250be090d93a9b13321016a64834e9001b6d
vikash sharma <vk220783@gmail.com>
2025-10-26 22:03:25 +0530

    creating sibling elements in react

--- namsteReactjs/day_01/app.js
+++ namsteReactjs/day_01/app.js
@@ -1,10 +1,52 @@
+
+// const heading = React.createElement('h1', {
+//     // this object is for attributes
+//     id: 'heading'
+// }, 'hello world from react js');
+// // now creating root using reactDom
+
+
+// // it will return an javascript object which is react element 
+// console.log(heading)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// // now rendering the heading using root
+
+// root.render(heading);
+
+// question : create a div which contains a h1 tag using react.createElement
+
+// div >div > h1 
+
+// creating heading element 
 const heading = React.createElement('h1', {
-    // this object is for attributes
     id: 'heading'
-}, 'hello world from react js');
-// now creating root using reactDom
+}, 'hello world from react js')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// now rendering the heading using root
+const heading2 = React.createElement('h1', {
+    id: 'heading'
+}, 'this is sibling of heading')
 
-root.render(heading);
+
+// creating a div which will contain heading
+
+const div1 = React.createElement('div', {
+    id: 'child'
+}, [
+    heading, heading2
+])
+
+// div1 will contain two heading as children - array of children 
+
+// creating main div which will contain div1
+
+const div = React.createElement('div', {
+    id: 'main'
+}, div1)
+
+
+// creating root
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// rendering the div
+root.render(div)
